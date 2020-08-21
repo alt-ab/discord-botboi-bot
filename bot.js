24cce163578591162df87abd2b3081ce9fac1236
Bit <ba6189641@gmail.com>
2020-08-20 20:00:29 -0400

    added giphy

--- bot.js
+++ bot.js
@@ -1,16 +1,24 @@
 require('dotenv').config();
 
+//Discord
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log(`Blast off! Logged in as ${client.user.tag}!`);
-});
+//Giphy
+var GphApiClient = require('giphy-js-sdk-core');
+const GIPHY_TOKEN = process.env.GIPHY_TOKEN;
+giphy = GphApiClient(GIPHY_TOKEN);
 
+//set up
 const PORT = process.env.PORT || 3000;
 
 client.on(PORT, () => {
     console.log(`App is running on port ${ PORT }`);
+});
+
+//output & code
+client.on('ready', () => {
+  console.log(`Blast off! Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
@@ -19,6 +27,7 @@ client.on('message', msg => {
   }
 });
 
-client.login(process.env.DISCORD_TOKEN);
 
+//bottom
+client.login(process.env.DISCORD_TOKEN);
 // -p $PORT for Procfile
