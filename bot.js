440f23be5c97ee4935acd02968c2796938dd4209
Bit <ba6189641@gmail.com>
2020-10-12 08:17:40 -0400

    riddlefix

--- bot.js
+++ bot.js
@@ -121,7 +121,9 @@ client.on('message', message => {
 
   if (message.content.includes (`${riddlefix}hi im dad`)) {
     //message.channel.type === (`"dm"`) +
-        message.author.send(`Congrats, on to the next the riddle!` + `Walk but can't run, x and y direction have WHAT THING in common?`)
+        message.author.send(`Congrats, on to the next the riddle!` + `Walk but can't run, x and y direction have WHAT THING in common?`).catch (() => {
+            message.console.send(`Error`);
+        })
   }
 
 });
