85c58a3772904d07a30daef1abc75806994c9b95
Bit <ba6189641@gmail.com>
2020-10-12 10:24:55 -0400

    riddler

--- bot.js
+++ bot.js
@@ -125,14 +125,14 @@ client.on('message', message => {
         })
   }
 
- // if (message.content.startsWith (`${prefix}hint`)) {
- //     if(message.member.roles.find(x => x.name === "riddler")) {
- //           message.channel.send('hi im blank')
- //     } 
- //     else {
- //         message.channel.send('Sorry you need to ask for the @riddler role')
- //     }
-});
+  if (message.content.startsWith (`${prefix}hint`)) {
+      if(message.member.roles.find('id', '744775211924062339')) {
+            message.channel.send('hi im blank')
+      } 
+      else {
+          message.channel.send('Sorry you need to ask for the @riddler role')
+      }
+};
 
 
 //bottom
