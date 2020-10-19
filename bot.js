96039b6c61851a862b8483cfed7c6d243f079d43
Bit <ba6189641@gmail.com>
2020-10-19 18:41:57 -0400

    commands

--- bot.js
+++ bot.js
@@ -93,10 +93,10 @@ client.on('message', message => {
       embed.setAuthor(client.user.username, client.user.displayAvatarURL());
       embed.setTitle("Commands");
       embed.setColor('00ffcc');
-      embed.addField("Botboi Commands w/ ``bb`` prefix", "**ping, echo, kick, dm, link, *help***");
-      embed.addField("Meme Commands", "bbmeme <meme>");
-      embed.addField("Streaming/Coding", "More hours to come! DM my mom if you want to display your stream times");
-      embed.addField("Riddle Commands w/ ``bb?``", "riddles **coming soon** hint1");
+      embed.addField("Botboi Commands w/ ``bb`` prefix", "**|help & commands| ping, echo, kick, dm me, link*");
+      embed.addField("Meme Commands w/ ``bbmeme`` prefix", "bbmeme <meme>");
+      embed.addField("Streaming/Coding Schedules", "More hours to come! use ``bbhelp`` & DM if you want to display your stream times");
+      embed.addField("Riddle Commands w/ ``bb?`` prefix", "riddles, & **coming soon** hint1");
 
       message.channel.send(embed);
   }
