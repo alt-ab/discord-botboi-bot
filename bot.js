8e692b6a5ba0d84e5bcf7d91d90bf4cf0e8331e5
unknown <ba6189641@gmail.com>
2021-03-11 14:03:16 -0500

    Added secret command outside of Regex

--- bot.js
+++ bot.js
@@ -70,6 +70,20 @@ client.on('message', message => {
         message.channel.send(explain);
     }
 
+    //for secret sing command
+    if (message.content.startsWith('bbsing')) {
+
+      const banner = "Happy-birthday!";
+
+      message.channel.send("**SPAM WARNING**");
+          //ooo look, you are using the join()
+      
+
+      for (let i = 0; i < banner.length; i++) {
+          message.channel.send(banner.charAt(i));
+      }
+    }
+
 ///
 //tutorial discordjs.guide/creating-your-bot/... 
 
@@ -143,154 +157,7 @@ client.on('message', message => {
 	message.reply('There was an error trying to execute that command!');
   }
 
-///
-
-
-//   if (message.member.hasPermission('KICK_MEMBERS')) {
-      
-//       if(message.content.startsWith(`${prefix}kick`)) {
-//         let member = message.mentions.members.first();
-//           if (!args.size) {
-//               return message.reply("You need to @ somebody!")
-//           }
-
-//           member.kick().then((member) => {
-
-//               giphy.search('gifs',{"q": "fail"})
-//                   .then((response) => {
-//                       var totalResponses = response.data.length;
-//                       var responseIndex = Math.floor((Math.random() * 10) + 1) % totalResponses;
-//                       var responseFinal = response.data[responseIndex];
-//                       //* code^^ for randomizing
-//                       message.channel.send(":wave: " + member.displayName + " is a dropout!!", {
-//                           files: [responseFinal.images.fixed_height.url]
-//                       })
-//                   })
-//           });
-          
-      
-//       }
-          
-//   }
-
-//   if (message.content.includes(`${prefix}dm me`)) {
-//     message.author.send(`Yessir!`);
-//   }
-
-//   if (message.content.includes(`${prefix}help`)) {
-//       let help = new Discord.MessageEmbed();
-//       help.setAuthor(client.user.username, client.user.displayAvatarURL());
-//       help.setAuthor = (client.user.username, client.user.displayAvatarURL());
-//       help.setTitle("Help!");
-//       help.setDescription("DM me! Not the bot me, but the coder @b_ambi#0749");
-//       help.setColor('00ffcc');
-
-//       message.channel.send(help);
-//   }
-
-//   if (message.content.startsWith(`${prefix}commands`)) {
-//       const embed = new Discord.MessageEmbed();
-
-//       embed.setAuthor(client.user.username, client.user.displayAvatarURL());
-//       embed.setTitle("Commands");
-//       embed.setColor('00ffcc');
-//       embed.addField("Botboi Commands w/ ``bb <command>``", "commands: **help, commands, invite**, ping, echo, kick, dm me, link");
-//       embed.addField("Meme Commands w/ ``bbmeme <command>``", "commands: meme");
-//       embed.addField("Streaming/Coding Schedules", "More hours to come! use ``bbhelp`` & DM if you want to display your stream times");
-//       embed.addField("Riddle Commands w/ ``bb? <command>``", "commands: riddles, & **coming soon** hint1");
-
-//       message.channel.send(embed);
-//   }
-
-//   if (message.content.startsWith(`${prefix}suggest`)) {
-//       console.log(message.content.substring(10));
-//   }
-
-//   if (message.content.startsWith(`${prefix}invite`)) {
-//       message.channel.send("https://discord.com/api/oauth2/authorize?client_id=702301294802501782&permissions=2147483639&scope=bot")
-//   }
-
-  //, {
-  //    files: [C:\Users\ab420\Desktop\python and code\discord-botboi-bot\R2-D2-512.png]
-  //}
-//   if (message.content.startsWith(`${prefix}ping`) || message.content.includes('Ping')) {
-//       message.channel.send(":ping_pong: pong!")
-//   }
-
-//   if (message.content.includes(`${prefix}link`)) {
-//       var amie = "Kawaii To Do list ";
-//       var result = "https://amie.so/w/?ref=LDSEWM6YX ";
-//       message.channel.send(":D " + amie + result);
-//   }
-
-//   if (message.content.startsWith(`${memefix} `)) {
-//       if (message.content.length)
-//       //If message content DOES NOT equal nothing then 
-
-//         giphy.search('gifs', {"q":message.content})
-//         .then((response) => {
-//             var totalResponses = response.data.length;
-//             var responseIndex = Math.floor((Math.random() * 10) + 1) % totalResponses;
-//             var responseFinal = response.data[responseIndex];
-
-//             message.channel.send({
-//                 files: [responseFinal.images.fixed_height.url]
-//             })
-//         }).catch(() => {
-//             message.channel.send('Error sorry');
-//         })
-//   }
-
-//   if (message.content.includes(`${prefix}echo`)) {
-//       message.channel.send(message.content.replace('botboi echo', ':microphone:'))
-//   }
-
-//   //! add '=>' in between the ) and the { ?
-//   if (message.content.startsWith(`${memefix}kpop`) || message.content.includes('bb jen')) {
-
-//     giphy.search('gifs', {"q":"kpop"})
-//     .then((response) => {
-//         var totalResponses = response.data.length;
-//         var responseIndex = Math.floor((Math.random() * 10) + 1) % totalResponses;
-//         var responseFinal = response.data[responseIndex];
-
-//         message.channel.send({
-//             files: [responseFinal.image.fixed_height.url]
-//         })
-//     }).catch(() => {
-//         message.channel.send('One sec');
-//     })
-//  }
-//Riddle me this!
-  //if (msg.content === `"Your message!"`) {
-  //  msg.channel.type === (`"dm"`) + msg.author.sendMessage(`"Your other message"`);
-  //}
- 
-//   if (message.content.includes (`${riddlefix}hi im dad`) || message.content.includes ("? ur mom")) {
-//         message.author.send(`Congrats, on to the next the riddle!` + ` | Walk but can't run, x and y direction have WHAT THING in common? |` ).catch (() => {
-//             message.console.send(`Error`);
-//         })
-//   }
-
 });
 
 client.login(process.env.DISCORD_TOKEN);
 
-
-// -p $PORT for Procfile
-// and because I'm lazy 
-// Git add .; Git commit -m "commit message"
-
-//*if(message.member.roles.has(role.id)) {
-    // user has that role
-  //} else {
-    //user doesn't have that role
-  //}
-
-  //if (command === "commandname"){
-   // let allowedRole = message.guild.roles.find("name", "rolename");
-   // if (message.member.roles.has(allowedRole.id) {
-        // allowed access to command
-   // } else {
-       // not allowed access
-   // // }
