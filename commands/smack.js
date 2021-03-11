d98db846e252d938437594b0fd618de47754aedc
unknown <ba6189641@gmail.com>
2021-03-11 14:01:43 -0500

    Fixed mentions variable to fit current Discord

--- commands/smack.js
+++ commands/smack.js
@@ -4,25 +4,32 @@ giphy = GphApiClient(GIPHY_TOKEN);
 
 module.exports = {
     name: "smack",
+    description: 'Smack your enemies',
     args: true,
     usage: '<user>',
-    description: 'Smack your enemies',
     execute(message, args) {
+        // if (!message.mentions.users.size) {
+        //     return message.channel.send("You need to call out people!")
+        // }
+  
+        // const callOutList = message.mentions.users.map(
+        //     user => {
+        //         return `${user.tag}`;
+        //     });
+
+        // message.channel.send(callOutList);
+
         let member = message.mentions.members.first();
 
-        if (!args.size) {
-            return message.reply("You need to @ somebody!")
-        }
-
         giphy.search('gifs',{"q": "hit"})
-            .then((response) => {
-                var totalResponses = response.data.length;
-                var responseIndex = Math.floor((Math.random() * 10) + 1) % totalResponses;
-                var responseFinal = response.data[responseIndex];
-                //* code^^ for randomizing
-                message.channel.send(`:wave: ${member.displayName} You have been smacked by ${message.author}`, {
-                    files: [responseFinal.images.fixed_height.url]
-                })
-            })
+             .then((response) => {
+                 var totalResponses = response.data.length;
+                 var responseIndex = Math.floor((Math.random() * 10) + 1) % totalResponses;
+                 var responseFinal = response.data[responseIndex];
+                 //* code^^ for randomizing
+                 message.channel.send(":wave: " + member.displayName + `You have been smacked by ${message.author}`, {
+                     files: [responseFinal.images.fixed_height.url]
+                 })
+             })
     },
 };
